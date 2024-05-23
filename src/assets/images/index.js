6ff145912bac21fd80b57204c4b12b2fd6309bd7
hs1enn <02jamesnathan@gmail.com>
2024-05-24 02:25:18 +0800

    Initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,159 @@
+import logo from "./VersantLogo.png";
+import logoLight from "./logoLight.png";
+import bannerImgOne from "./banner/bannerImgOne.png";
+import bannerImgTwo from "./banner/bannerImgTwo.png";
+import bannerImgThree from "./banner/bannerImgThree.png";
+import saleImgOne from "./sale/saleImgOne.png";
+import saleImgTwo from "./sale/saleImgTwo.png";
+import saleImgThree from "./sale/saleImgThree.png";
+// ============== Products Start here ====================
+import pdf1 from "../pdf/pdf1.pdf";
+// New Arrivals
+import newArrOne from "./products/newArrival/newArrOne.png";
+import newArrTwo from "./products/newArrival/newArrTwo.png";
+import newArrThree from "./products/newArrival/newArrThree.png";
+import newArrFour from "./products/newArrival/newArrFour.png";
+import newArrFive from "./products/newArrival/newArrFive.png";
+
+// Best Sellers
+import bestSellerOne from "./products/bestSeller/bestSellerOne.png";
+import bestSellerTwo from "./products/bestSeller/bestSellerTwo.png";
+import bestSellerThree from "./products/bestSeller/bestSellerThree.png";
+import bestSellerFour from "./products/bestSeller/bestSellerFour.png";
+import phone1 from "./products/bestSeller/phone1.png";
+import phone2 from "./products/bestSeller/phone2.png";
+import phone4 from "./products/bestSeller/phone4.png";
+import phone5 from "./products/bestSeller/phone6.png";
+import phone6 from "./products/bestSeller/phone7.png";
+import phone7 from "./products/bestSeller/phone10.png";
+import phone8 from "./products/bestSeller/phone11.png";
+import phone9 from "./products/bestSeller/phone13.png";
+import laptop1 from "./products/bestSeller/laptop1.png";
+import laptop2 from "./products/bestSeller/laptop2.png";
+import laptop3 from "./products/bestSeller/laptop3.png";
+import laptop4 from "./products/bestSeller/laptop4.png";
+import mouse1 from "./products/bestSeller/mouse1.png";
+import mouse2 from "./products/bestSeller/mouse2.png";
+import mouse3 from "./products/bestSeller/mouse3.png";
+import mouse4 from "./products/bestSeller/mouse4.png";
+import headset1 from "./products/bestSeller/headset1.png";
+import headset2 from "./products/bestSeller/headset2.png";
+import headset3 from "./products/bestSeller/headset3.png";
+import headset4 from "./products/bestSeller/headset4.png";
+
+// Special Offers
+import spfOne from "./products/specialOffer/spfOne.webp";
+import spfTwo from "./products/specialOffer/spfTwo.webp";
+import spfThree from "./products/specialOffer/spfThree.webp";
+import spfFour from "./products/specialOffer/spfFour.webp";
+
+// Year Product
+import productOfTheYear from "./products/productOfTheYear.webp";
+// ============== Products End here ======================
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+
+// import phone1 from "../images/products/imprimante/phone1.png";
+// import phone2 from "../images/products/imprimante/phone2.png";
+// import phone4 from "../images/products/imprimante/phone3.png";
+// import phone5 from "../images/products/imprimante/phone4.png";
+// import phone6 from "../images/products/imprimante/phone5.png";
+// import phone7 from "../images/products/imprimante/phone6.png";
+// import phone8 from "../images/products/imprimante/phone7.png";
+// import phone9 from "../images/products/imprimante/phone8.png";
+import phone10 from "../images/products/bestSeller/phone3.png";
+
+import cam1 from "../images/products/imprimante/cam1.png";
+import cam2 from "../images/products/imprimante/cam2.png";
+import cam3 from "../images/products/imprimante/cam3.png";
+
+import ricoh1 from "../images/products/imprimante/ricoh1.webp";
+import ricoh2 from "../images/products/imprimante/ricoh2.webp";
+import ricoh3 from "../images/products/imprimante/ricoh3.webp";
+import ricoh4 from "../images/products/imprimante/ricoh4.webp";
+import ricoh5 from "../images/products/imprimante/ricoh5.webp";
+
+import espson1 from "../images/products/imprimante/espson1.webp";
+import espson2 from "../images/products/imprimante/espson2.webp";
+
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  // ===================== Products start here ============
+  // New Arrivals
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+  newArrFive,
+
+  // Best Sellers
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+  phone1,
+  phone2,
+  phone4,
+  phone5,
+  phone6,
+  phone7,
+  phone8,
+  phone9,
+  laptop1,
+  laptop2,
+  laptop3,
+  laptop4,
+  mouse1,
+  mouse2,
+  mouse3,
+  mouse4,
+  headset1,
+  headset2,
+  headset3,
+  headset4,
+
+  // Sprcial Offers
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+
+  // Year Product
+  productOfTheYear,
+  // ===================== Products End here ==============
+  paymentCard,
+  emptyCart,
+  pdf1,
+
+  // imprimante pantum
+  // phone1,
+  // phone2,
+  // phone4,
+  // phone5,
+  // phone6,
+  // phone7,
+  // phone8,
+  // phone9,
+  phone10,
+
+  // imprimante Hp
+  cam1,
+  cam2,
+  cam3,
+
+  // imprimante Ricoh
+  ricoh1,
+  ricoh2,
+  ricoh3,
+  ricoh4,
+  ricoh5,
+  espson1,
+  espson2,
+};
