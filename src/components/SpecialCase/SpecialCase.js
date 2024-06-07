78dfaaeccc587085ddcc91623f91f515bafd4677
hs1enn <02jamesnathan@gmail.com>
2024-06-07 16:03:05 +0800

    Special Case Responsive

--- src/components/SpecialCase/SpecialCase.js
+++ src/components/SpecialCase/SpecialCase.js
@@ -7,12 +7,11 @@ import { useSelector } from "react-redux";
 const SpecialCase = () => {
   const products = useSelector((state) => state.orebiReducer.products);
   return (
-    <div className="fixed top-52 right-2 z-20 hidden md:flex flex-col gap-2">
+    <div className="fixed top-52 right-2 z-20 flex flex-col gap-2 md:flex md:gap-2">
       <Link to="/journal2">
         <div className="bg-white w-16 h-[70px] rounded-md flex flex-col gap-1 text-[#33475b] justify-center items-center shadow-testShadow overflow-x-hidden group cursor-pointer">
           <div className="flex justify-center items-center">
             <MdSwitchAccount className="text-2xl -translate-x-12 group-hover:translate-x-3 transition-transform duration-200" />
-
             <MdSwitchAccount className="text-2xl -translate-x-3 group-hover:translate-x-12 transition-transform duration-200" />
           </div>
           <p className="text-xs font-semibold font-titleFont">Profile</p>
@@ -22,7 +21,6 @@ const SpecialCase = () => {
         <div className="bg-white w-16 h-[70px] rounded-md flex flex-col gap-1 text-[#33475b] justify-center items-center shadow-testShadow overflow-x-hidden group cursor-pointer relative">
           <div className="flex justify-center items-center">
             <RiShoppingCart2Fill className="text-2xl -translate-x-12 group-hover:translate-x-3 transition-transform duration-200" />
-
             <RiShoppingCart2Fill className="text-2xl -translate-x-3 group-hover:translate-x-12 transition-transform duration-200" />
           </div>
           <p className="text-xs font-semibold font-titleFont">Buy Now</p>
